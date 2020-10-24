5d175143d9dea25528b9b411676492f78ad74173
Dmitri <Dmitri.Danshin2017@gmail.com>
2020-10-24 21:21:01 +0300

    add server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 80;
+
+app.get('/',(req,res)=>{
+    res.end('dmitri5');
+})
+
+app.get('/login',(req,res)=>{
+    res.end('dmitri5');
+})
+
+app.listen(PORT, ()=>{
+    console.log("successful")
+});
